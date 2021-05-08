157defff78c730660befc1fa7fac6e447399503b
arodrigoo900045 <adilsonmissionario@gmail.com>
2021-05-08 19:42:38 +0100

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,98 @@
+const DataTable = () => {
+    return (
+    <div className="table-responsive">
+        <table className="table table-striped table-sm">
+            <thead>
+                <tr>
+                    <th>Data</th>
+                    <th>Vendedor</th>
+                    <th>Clientes visitados</th>
+                    <th>Negócios fechados</th>
+                    <th>Valor</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Barry Allen</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Elison Gomes</td>
+                    <td>34</td>
+                    <td>22</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Eduardo Pedro</td>
+                    <td>34</td>
+                    <td>20</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Mary Bonito</td>
+                    <td>34</td>
+                    <td>17</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Victor Silva</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Paulo Eduardo</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Mister Koyayas</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Sudre Kimuanga</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Mielvan Rodrigo</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Samuel Epalanga</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+                <tr>
+                    <td>22/04/2021</td>
+                    <td>Ricardo Semedo</td>
+                    <td>34</td>
+                    <td>25</td>
+                    <td>15017.00</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    );
+  }
+  
+  export default DataTable;
