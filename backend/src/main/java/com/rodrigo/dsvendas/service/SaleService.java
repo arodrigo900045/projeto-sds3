5dcb283c3e986cd9c930a80157acc037a9597fb3
arodrigoo900045 <adilsonmissionario@gmail.com>
2021-05-09 21:53:16 +0100

    Pagination

---
+++ backend/src/main/java/com/rodrigo/dsvendas/service/SaleService.java
@@ -0,0 +1,30 @@
+package com.rodrigo.dsvendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rodrigo.dsvendas.dto.SaleDTO;
+import com.rodrigo.dsvendas.entities.Sale;
+import com.rodrigo.dsvendas.repositories.SaleRepository;
+import com.rodrigo.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService 
+{  
+   @Autowired	
+   private SaleRepository repository;
+   
+   @Autowired
+   private SellerRepository sellerRepository;
+   
+   @Transactional(readOnly = true)
+   public Page<SaleDTO> findAll(Pageable pageable)
+   {
+	   sellerRepository.findAll();
+	   Page<Sale> result = repository.findAll(pageable);
+	   return result.map(x -> new SaleDTO(x));
+   }
+}
